575574e617832d1920872a6f1f7e84901eee45b5
Robert West <robertmathewwest10@gmail.com>
2021-11-09 18:53:10 -0500

    first commit

---
+++ pages/buscador.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Layout from "../components/layout/Layout";
+
+const Buscador = () => {
+  return (
+    <div>
+      <Layout>
+        <h1>Buscador</h1>
+      </Layout>
+    </div>
+  );
+};
+
+export default Buscador;
